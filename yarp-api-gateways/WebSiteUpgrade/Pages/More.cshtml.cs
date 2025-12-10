26863a58de28f5ba7b05a663c772a40be64cd57f
Javed Ahmad <javedfgiet@gmail.com>
2025-12-10 21:52:02 +0530

    Features/yarp setup (#3)
    * Add OptimisticApi, WebSiteUpgrade, and YarpGateWays projects
    
    Introduces three new projects: OptimisticApi (with a weather forecast endpoint), WebSiteUpgrade (similar weather API), and YarpGateWays (reverse proxy using YARP). Updates PessimisticApi to restrict weather summaries and temperature range, and modifies launch settings. Solution file updated to include new projects.
    
    * Added yar code

---
+++ yarp-api-gateways/WebSiteUpgrade/Pages/More.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebSiteUpgrade.Pages
+{
+    public class MoreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
